bb6070cb302dd44f7c6989112396628ce124273f
Selin <selin.barbulescu@gmail.com>
2026-07-31 19:21:05 +0300

    Update web service plan tier

--- terraform/modules/web_app/main.tf
+++ terraform/modules/web_app/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_service_plan" "service_plan" {
   resource_group_name = azurerm_resource_group.resource_group.name
   location            = azurerm_resource_group.resource_group.location
   os_type             = "Linux"
-  sku_name            = "P1v2"
+  sku_name            = "F1"
 }
 
 resource "azurerm_linux_web_app" "web_app" {
@@ -19,6 +19,6 @@ resource "azurerm_linux_web_app" "web_app" {
   service_plan_id     = azurerm_service_plan.service_plan.id
 
   site_config {
-
+    always_on = false
   }
 }
